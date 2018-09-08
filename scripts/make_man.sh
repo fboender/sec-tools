e0f1f1afa5e6a6fd4de24fb959576137463884ff
Ferry Boender <ferry.boender@gmail.com>
2018-09-08 10:25:31 +0200

    Remove 'Automatically generated' pandoc header from manpages.

--- scripts/make_man.sh
+++ scripts/make_man.sh
@@ -10,4 +10,5 @@ for MD_MAN_FILE in $MAN_DIR/*.md; do
     echo "Converting $(basename $MD_MAN_FILE)"
     MAN_FILE=$(basename $MD_MAN_FILE .md)
     pandoc $MD_MAN_FILE -s -t man > $MAN_DIR/$MAN_FILE
+    sed "/Automatically generated/,+1d" -i "$MAN_DIR/$MAN_FILE" 
 done
