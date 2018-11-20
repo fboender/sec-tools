5313abba2612951e6be7681772df27f53b9e1d89
Ferry Boender <ferry.boender@gmail.com>
2018-11-20 07:19:51 +0100

    sec-gather-misconfigs: Additional net tests.

--- src/sec-gather-misconfigs.d/net.py
+++ src/sec-gather-misconfigs.d/net.py
@@ -5,6 +5,66 @@ import tools
 import urllib2
 
 
+def _file_content_is(result, path, content):
+    with open(path, 'r') as f:
+        if f.read().strip() == content:
+            result.passed(True)
+            result.add_result("'{}' content is '{}'".format(path, content))
+        else:
+            result.passed(False)
+            result.add_result("'{}' content is not '{}'".format(path, content))
+    return result
+
+def tcp_sycookies_disabled():
+    result = Result(
+        desc="TCP SYN cookies should be enabled",
+        explanation="TCP SYN cookie is a technique used to resist SYN flood attacks",
+        severity=2,
+        passed=False
+    )
+
+    return _file_content_is(result, '/proc/sys/net/ipv4/tcp_syncookies', '1')
+
+def accept_source_route():
+    result = Result(
+        desc="Source Routing should not be allowed",
+        explanation="Source Routing is used to specify a path or route through the network from source to destination. This feature can be used by network people for diagnosing problems. However, if an intruder was able to send a source routed packet into the network, then he could intercept the replies and your server might not know that it's not communicating with a trusted server.",
+        severity=3,
+        passed=False
+    )
+
+    return _file_content_is(result, '/proc/sys/net/ipv4/conf/all/accept_source_route', '0')
+
+def icmp_ignore_broadcast():
+    result = Result(
+        desc="ICMP broadcasts should be ignored",
+        explanation="Machines should not reply to ICMP ping broadcast requests.",
+        severity=1,
+        passed=False
+    )
+
+    return _file_content_is(result, '/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts', '1')
+
+def icmp_redirects():
+    result = Result(
+        desc="ICMP redirects should not be allowed",
+        explanation="ICMP redirects are used by routers to tell the server that there is a better path to other networks than the one chosen by the server. However, an intruder could potentially use ICMP redirect packets to alter the hosts's routing table by causing traffic to use a path you didn't intend.",
+        severity=3,
+        passed=False
+    )
+
+    return _file_content_is(result, '/proc/sys/net/ipv4/conf/all/accept_redirects', '0')
+
+def ip_spoofing_protection():
+    result = Result(
+        desc="IP spoofing protection should be enabled",
+        explanation="",
+        severity=3,
+        passed=False
+    )
+
+    return _file_content_is(result, '/proc/sys/net/ipv4/conf/all/rp_filter', '1')
+
 def hostname():
     result = Result(
         desc="Invalid (fully qualified) hostname",
