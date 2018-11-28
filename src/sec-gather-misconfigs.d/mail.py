442c7b03c3f2ae8c2561e53516db0280159eba8c
Ferry Boender <ferry.boender@gmail.com>
2018-11-28 08:51:51 +0100

    mail: check spool dir

---
+++ src/sec-gather-misconfigs.d/mail.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import tools
+
+
+def mail_spool_populated():
+    result = Result(
+        desc="No mail should arrive in /var/spool/mail",
+        explanation="Mail that arrives in /var/spool/mail is generally never read, and indicated missing mail forwarding settings for users.",
+        severity=5,
+        passed=True
+    )
+
+    spool_dir = '/var/spool/mail'
+    if os.path.isdir(spool_dir):
+        for fname in os.listdir(spool_dir):
+            path = os.path.join(spool_dir, fname)
+            st = os.stat(path)
+            if st.st_size > 0:
+                result.passed = True
+                result.add_result("{} size is 0".format(path))
+    else:
+        result.add_result("{} not found".format(spool_dir))
+
+    return result
